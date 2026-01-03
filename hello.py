091063b62addbdfbd1e376cedc9b414994087810
Pravalika <npravalika059@gmail.com>
2026-01-03 21:45:14 +0530

    Updated readme

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 def greet():
     print("Hello, World!")
-=======
+
 import pandas as pd
 import numpy as np
 
 def greet():
     print("Hello World!")
->>>>>>> python
+
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 This is very good information
+
+This is basics daily routine of humans:
+-Eat
+-Do something
+-Sleep
+
+Thank yoy!
